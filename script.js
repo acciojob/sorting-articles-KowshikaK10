6b8444bb05f5b84af9219a57ecceb1bcea97136b
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-06 16:06:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-//your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-function replaced(a){
-	return a.replace(/^(a |an |the )/i, '').trim();
+const bands = [
+    'The Plot in You', 
+    'The Devil Wears Prada', 
+    'Pierce the Veil', 
+    'Norma Jean', 
+    'The Bled', 
+    'Say Anything', 
+    'The Midway State', 
+    'We Came as Romans', 
+    'Counterparts', 
+    'Oh, Sleeper', 
+    'A Skylit Drive', 
+    'Anywhere But Here', 
+    'An Old Dog'
+];
+function replaced(article) {
+    return article.replace(/^(a |an |the )/i, '').trim();
 }
-let sortedArr=bands.sort((a,b)=>replaced(a).localeCompare(replaced(b)));
-const ul=document.getElementById('band')
-sortedArr.forEach((sorted)=>{
-		let li=document.createElement('li');
-		li.textContent=sorted;
-		ul.appendChild(li);
+const sortedBands = bands.sort((a, b) => replaced(a).localeCompare(replaced(b)));
+const ul = document.getElementById('band');
+sortedBands.forEach(sortBand => {
+    const li = document.createElement('li');
+    li.textContent = sortBand;
+    ul.appendChild(li);
 });
